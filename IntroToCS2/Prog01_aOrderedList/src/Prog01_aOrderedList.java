60a224bd3c2e733cc85a2042cb95568026bb1849
johnholcomb10 <160180610+johnholcomb10@users.noreply.github.com>
2024-03-08 18:09:39 -0600

    3/7 Programming Assignment Update

--- IntroToCS2/Prog01_aOrderedList/src/Prog01_aOrderedList.java
+++ IntroToCS2/Prog01_aOrderedList/src/Prog01_aOrderedList.java
@@ -1,7 +1,30 @@
 import java.util.*;
+import java.io.*;
 public class Prog01_aOrderedList {
 	
 	public static void main(String[] args) {
+		boolean hasFile;
+		Scanner fileInput = new Scanner(System.in);
 		
+		System.out.println("Enter input filename, or type \"done\" to cancel the program:");
+		
+		try{
+			Scanner reader = getInputFile(fileInput.next());
+			hasFile = true;
+			fileInput.close();
+		} catch (Exception FileNotFoundException) {
+			System.out.println("File not found! Try again!\nEnter input filename, or type \"done\" to cancel the program: ");
+		}
 	}
 
+public static Scanner getInputFile(String fileName) throws FileNotFoundException {
+	if(fileName.toLowerCase().equals("done")) {
+		System.out.println();
+		System.exit(0);
+	}
+	File readFile = new File(fileName);
+	Scanner reader = new Scanner(readFile);
+	return reader;
+}
+
+}
--- IntroToCS2/Prog01_aOrderedList/src/aOrderedList.java
+++ IntroToCS2/Prog01_aOrderedList/src/aOrderedList.java
@@ -1,20 +1,44 @@
 import java.util.*;
-public class aOrderedList implements Comparable<Car>{
+public class aOrderedList{
 	
 	final int SIZEINCREMENTS = 20;
-	private Car[] oList;
+	private ArrayList<Car> oList;
 	private int listSize;
 	private int numObjects;
 	
 	public aOrderedList() {
-		oList = new Car[SIZEINCREMENTS];
+		oList = new ArrayList<>(SIZEINCREMENTS);
 		listSize = SIZEINCREMENTS;
 		numObjects = 0;
 	}
 	
 	public void add(Car newCar) {
-		oList[numObjects] = newCar;
+		oList.add(numObjects, newCar);
 		numObjects++;
 		Collections.sort(oList);
 	}
+
+	public int size() {
+		return listSize;
+	}
+
+	public Car get(int index) {
+		return oList.get(index);
+	}
+
+	public boolean isEmpty() {
+		if(numObjects == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public void remove(int index) {
+		oList.remove(index);
+		for(int i = (index + 1); i<oList.size();i++){
+			oList.
+		}
+		numObjects--;
+	}
+
 }
